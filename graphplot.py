080e1e6f363bf795c58055e035b3cb1891d0cf22
Annie <73708386+annietjx@users.noreply.github.com>
2023-03-28 23:38:54 -0400

    updated docstrings

--- graphplot.py
+++ graphplot.py
@@ -9,31 +9,52 @@ class GraphPlot:
     """
     A GraphPlot class that contains methods to plot graphs to analyse the AI’s performance against
     a TrainingAgent instance or a trained AIPlayer instance.
+    
+    Instance Attributes:
+        - results: A list of winners of each game, consisting of either (player) 1 or 2. A 0 indicates a draw.
+        - ai_player: The player number indicating the AI player.
+
+    Representation Invariants:
+        - all(num in {0, 1, 2} for num in self.results)
+        - ai_player in {1, 2}
     """
     results: list[int]
+    ai_player: int
 
-    def __init__(self, results: list[int]) -> None:
+    def __init__(self, results: list[int], ai: int) -> None:
+        """Initalize a new GraphPlot instance using results.
+
+        Preconditions:
+            - len(results) > 0
+            - ai in {1, 2}
+        """
         self.results = results
+        self.ai_player = ai
 
     def plot_game_stats(self) -> None:
         """
-        Plot game statistics.
+        Plots the following game statistics:
+        1. Outcomes: a scatter plot showing the outcome of each game,
+        where 1 represents a win for player 1 and 0 represents a win for player 2.
+        2. Player 1 cumulative win percentages: a scatter plot showing the cumulative win percentage
+        for player 1 over all games played.
+        3. Number of wins: a bar plot showing the number of wins for player 1 and player 2.
         """
-        outcomes = [1 if result == 1 else 0 for result in self.results]
+        outcomes = self.results
         cumulative_win_percentage = [sum(outcomes[0:i]) / i for i in range(1, len(outcomes) + 1)]
-        num_of_wins = [self.results.count(1), self.results.count(2)]
+        num_of_wins = [self.results.count(1), self.results.count(2), self.results.count(0)]
 
         fig = make_subplots(rows=2, cols=2)
 
         fig.add_trace(go.Scatter(y=outcomes, mode='markers',
-                                 name='Outcome (1 = Player 1 win, 0 = Player 2 win)'),
+                                 name='Outcome (1 = Player 1 win, 0 = Player 2 win, 0 = Draw)'),
                       row=1, col=1)
 
         fig.add_trace(go.Scatter(y=cumulative_win_percentage, mode='lines',
                                  name='Player 1 win percentage (cumulative)'),
                       row=2, col=1)
 
-        fig.append_trace(go.Bar(x=['Player 1', 'Player 2'], y=num_of_wins,
+        fig.append_trace(go.Bar(x=['Player 1', 'Player 2', 'Draw'], y=num_of_wins,
                                 text=num_of_wins, textposition='auto',
                                 name='Number of wins for player 1 and 2'), row=1, col=2)
 
